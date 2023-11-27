c7fc727181404d714da5fdf9c5b92ed669bfe65c
leela5 <leela@secureitlab.com>
2023-11-27 10:53:18 +0530

    Push existing project to GitLab 123

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -232,4 +232,58 @@ class BookController extends Controller
         }
 
     }
+
+    public function dashboard(){
+        //  $images = ['9780385547345.jpg', '9781668026274.jpg', '9781668016138.jpg', '9780804172707.jpg', '9780590353427.jpg', '9781668007877.jpg', '9781338885132.jpg'  ,'9781649374042.jpg', '9780385548953.jpg'];
+
+        $books_data = Cache::remember('books_data1', 600, function () {
+            return Books::orderBy('created_at', 'desc')->get();
+        });
+
+        // $books = Books::all();
+
+        //  foreach( $books as  $book){
+
+        //   //  $book->image = '/images/' . $images[rand(0,8)];
+
+        //      $book->delete();
+        // }
+
+        //  dd(Cache::get('books_data1'));
+
+        //  $books_title =   $books_data->unique('title');
+
+        //  dd($books_title, $books_title[0]['title'] );
+
+        // foreach( $books as  $book){
+
+        //     $book->image = '/images/' . $images[rand(0,8)];
+
+        //      $book->update();
+        // }
+
+        // $json = file_get_contents('https://fakerapi.it/api/v1/books?_quantity=100');
+
+
+        // $objs = (array) json_decode($json);
+
+        // foreach($objs['data'] as $obj){
+
+        //    $books = new Books();
+
+        //    $books->title = $obj->title;
+        //     $books->author = $obj->author;
+        //     $books->genre = $obj->genre;
+        //     $books->description = $obj->description;
+        //     $books->image = '/images/' . $images[rand(0, 8)];
+        //     $books->published = $obj->published;
+        //     $books->publisher = $obj->publisher;
+
+        //     $books->save();
+        // }
+
+        //    dd($objs['data'][0]);
+
+        return view('dashboard', compact('books_data'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -41,64 +41,6 @@ Route::get('dial-pad', function () {
 Route::get('book-details/{id}', [BookController::class, 'book_details']);
 
 
-Route::get('/dashboard', function () {
-
-
-
-  //  $images = ['9780385547345.jpg', '9781668026274.jpg', '9781668016138.jpg', '9780804172707.jpg', '9780590353427.jpg', '9781668007877.jpg', '9781338885132.jpg'  ,'9781649374042.jpg', '9780385548953.jpg'];
-
-    $books_data = Cache::remember('books_data1', 600, function () {
-        return Books::orderBy('created_at', 'desc')->get();
-    });
-
-    // $books = Books::all();
-
-    //  foreach( $books as  $book){
-
-    //   //  $book->image = '/images/' . $images[rand(0,8)];
-
-    //      $book->delete();
-    // }
-
-  //  dd(Cache::get('books_data1'));
-
-  //  $books_title =   $books_data->unique('title');
-
-  //  dd($books_title, $books_title[0]['title'] );
-
-    // foreach( $books as  $book){
-
-    //     $book->image = '/images/' . $images[rand(0,8)];
-
-    //      $book->update();
-    // }
-
-    // $json = file_get_contents('https://fakerapi.it/api/v1/books?_quantity=100');
-
-
-    // $objs = (array) json_decode($json);
-
-    // foreach($objs['data'] as $obj){
-
-    //    $books = new Books();
-
-    //    $books->title = $obj->title;
-    //     $books->author = $obj->author;
-    //     $books->genre = $obj->genre;
-    //     $books->description = $obj->description;
-    //     $books->image = '/images/' . $images[rand(0, 8)];
-    //     $books->published = $obj->published;
-    //     $books->publisher = $obj->publisher;
-
-    //     $books->save();
-    // }
-
-//    dd($objs['data'][0]);
-
-
-
-
-    return view('dashboard', compact('books_data'));
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [BookController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
